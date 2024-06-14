76acca11521d3b0679392dc9c954a4c998e82117
Taiihor <boutaihor571@gmail.com>
2024-06-14 23:41:45 +0700

    update profile

--- lib/view/screen/booking/profile_screen.dart
+++ lib/view/screen/booking/profile_screen.dart
@@ -32,7 +32,6 @@ class _DriverProfileScreenState extends State<DriverProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         leading: IconButton(
           onPressed: () {
             Get.back();
@@ -58,207 +57,131 @@ class _DriverProfileScreenState extends State<DriverProfileScreen> {
   }
 
   Widget _buildBody() {
-    return SafeArea(
-        child: SingleChildScrollView(
-      child: Stack(
-        children: [
-          Column(
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: SafeArea(
+          child: Stack(
             children: [
-              Container(
-                  height: Get.height * 0.4,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        ColorResources.primaryColor.withOpacity(0.8),
-                        ColorResources.whiteBackgroundColor,
-
-                      ],
-                    ),
-                    color: ColorResources.primaryColor,
-                  )),
-              Container(
-                height: Get.height * 0.6,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      ColorResources.whiteBackgroundColor,
-                      ColorResources.primaryColor.withOpacity(0.2),
-                    ],
-                  ),
-                  color: ColorResources.whiteBackgroundColor,
-                ),
-              ),
-            ],
-          ),
-          Positioned(top: 40, left: 10, right: 10, child: _buildProfile()),
-        ],
-      ),
-    ));
-  }
-
-  //Todo : _buildProfile
-  Widget _buildProfile() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: Get.width * 0.9,
-        //height: Get.height,
-        decoration: BoxDecoration(
-          //color: ColorResources.whiteBackgroundColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              SizedBox(height: 16),
-              //Todo : Image Profile
-              _image == null
-                  ? Container(
-                      width: 100,
-                      height: 100,
+              Column(
+                children: [
+                  Container(
+                      height: Get.height * 0.4,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: CachedNetworkImageProvider(urlImagProfile),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    )
-                  : CircleAvatar(
-                      backgroundImage: Image.file(
-                        File(_image!.path),
-                      ).image,
-                      radius: 50,
-                    ),
-              //Todo: pickImage
-              TextButton.icon(
-                onPressed: () {
-                  Get.dialog(
-                    AlertDialog(
-                      title: Text(
-                        'Choose an option',
-                        style: TextStyle(color: ColorResources.primaryColor),
-                      ),
-                      content: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Divider(),
-                            TextButton.icon(
-                              onPressed: () {
-                                pickImage(ImageSource.gallery);
-                                Get.back();
-                              },
-                              icon: Icon(Icons.photo),
-                              label: Text("Select from Gallery"),
-                            ),
-                            Padding(padding: EdgeInsets.all(8.0)),
-                            TextButton.icon(
-                              onPressed: () {
-                                pickImage(ImageSource.camera);
-                                Get.back();
-                              },
-                              icon: Icon(Icons.camera_alt_outlined),
-                              label: Text("Take a Photo"),
-                            )
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            ColorResources.primaryColor.withOpacity(0.8),
+                            ColorResources.whiteBackgroundColor,
                           ],
                         ),
+                        color: ColorResources.primaryColor,
+                      )),
+                  Container(
+                    height: Get.height * 0.5,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          ColorResources.whiteBackgroundColor,
+                          ColorResources.primaryColor.withOpacity(0.2),
+                        ],
                       ),
-                    ),
-                  );
-                },
-                icon: Icon(Icons.camera_alt_outlined),
-                label: Text(
-                  'កែប្រែរូបភាព',
-                  style: TextStyle(
-                    color: ColorResources.primaryColor,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              //Todo: More information
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "ព័ត៌មានផ្ទាល់ខ្លួន",
-                    style: GoogleFonts.notoSerifKhmer(
-                      fontSize: 20,
-                      color: ColorResources.primaryColor,
+                      color: ColorResources.whiteBackgroundColor,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 32),
-              //Todo: ListTile of Profile
-              CustomListWidget.customListTile(
-                title: 'ឈ្មោះ',
-                iconleading: Icons.person,
-                onPress: () {},
-              ),
-              SizedBox(height: 16),
-              CustomListWidget.customListTile(
-                iconleading: Icons.email,
-                title: 'អ៊ីម៉ែល',
-                onPress: () {},
-              ),
-              SizedBox(height: 16),
-              CustomListWidget.customListTile(
-                title: 'លេខទូរស័ព្ទ',
-                iconleading: Icons.phone,
-                onPress: () {},
-              ),
-              SizedBox(height: 16),
-              CustomListWidget.customListTile(
-                title: 'ថ្ងៃខែឆ្នាំកំណត់',
-                iconleading: Icons.calendar_today,
-                onPress: () {},
-              ),
-              SizedBox(height: 16),
-              CustomListWidget.customListTile(
-                title: 'ភេទ',
-                iconleading: Icons.wc,
-                onPress: () {},
-              ),
-              SizedBox(height: 16),
-              CustomListWidget.customListTile(
-                title: 'អាសយដ្ឋាន',
-                iconleading: Icons.location_on,
-                onPress: () {},
-              ),
-              SizedBox(height: 32),
-             Container(
-              height: 60,
-              width: Get.width * 0.8,
-                padding: EdgeInsets.symmetric(horizontal: 32),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'កែប្រែព័ត៌មានផ្ទាល់ខ្លួន',
-                    style: TextStyle(
-                      color: ColorResources.redColor,
-                      fontSize: 22,
-                    ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    iconColor: ColorResources.primaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
-             ),
-
-              SizedBox(height: 16),
+              Positioned(top: 40, left: 10, right: 10, child: _buildProfile()),
             ],
+          )),
+    );
+  }
+
+  //Todo : _buildProfile
+  Widget _buildProfile() {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: Get.width * 0.9,
+          //height: Get.height,
+          decoration: BoxDecoration(
+            //color: ColorResources.whiteBackgroundColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 16),
+                  //Todo : Image Profile
+                  _image == null
+                      ? Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: CachedNetworkImageProvider(urlImagProfile),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      : CircleAvatar(
+                          backgroundImage: Image.file(
+                            File(_image!.path),
+                          ).image,
+                          radius: 50,
+                        ),
+
+                  //Todo: More information
+                  SizedBox(height: 32),
+                  //Todo: ListTile of Profile
+                  CustomListWidget.customListTile(
+                    title: 'ឈ្មោះ',
+                    iconleading: Icons.person,
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 16),
+                  CustomListWidget.customListTile(
+                    iconleading: Icons.email,
+                    title: 'អ៊ីម៉ែល',
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 16),
+                  CustomListWidget.customListTile(
+                    title: 'លេខទូរស័ព្ទ',
+                    iconleading: Icons.phone,
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 16),
+                  CustomListWidget.customListTile(
+                    title: 'ថ្ងៃខែឆ្នាំកំណត់',
+                    iconleading: Icons.calendar_today,
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 16),
+                  CustomListWidget.customListTile(
+                    title: 'ភេទ',
+                    iconleading: Icons.wc,
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 16),
+                  CustomListWidget.customListTile(
+                    title: 'អាសយដ្ឋាន',
+                    iconleading: Icons.location_on,
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 32),
+                ],
+              ),
+            ),
           ),
         ),
       ),
